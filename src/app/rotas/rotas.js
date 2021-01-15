4935aa579974da33001b8ba9c52a4afee312cb9c
lfdinisfernandes <54485828+lfdinisfernandes@users.noreply.github.com>
2021-01-14 22:10:24 -0300

    manippulando a requisição com middlewares

---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,51 @@
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
+const listaMarko = require('../views/livros/lista/lista.marko');
+
+module.exports = (app) => {
+    app.get('/', function(req, resp) {
+        resp.send(
+            `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Casa do Código </h1>
+            </body>
+        </html>
+       `
+        );
+    });
+
+    app.get('/livros', function(req, resp) {
+
+        const livroDao = new LivroDao(db);
+        livroDao.lista()
+            .then(livros => resp.marko(
+                require('../views/livros/lista/lista.marko'), {
+                    livros: livros
+                }
+            ))
+            .catch(erro => console.log(erro));
+        // livroDao.lista(function(erro, resultados) {
+
+        //     resp.marko(
+        //         require('../views/livros/lista/lista.marko'), {
+        //             livros: resultados
+        //         }
+        //     );
+
+        // });
+
+        // db.all('SELECT * FROM livros', function(erro, resultados) {
+
+        //   resp.marko(
+        //     require('../views/livros/lista/lista.marko'), {
+        //       livros: resultados
+        // }
+        // );
+
+        //});
+    });
+};
